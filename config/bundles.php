40525b31b6a6c5060f03f1989954b5d280e2b885
root <root@DESKTOP-GAVDRN5.localdomain>
2026-08-18 09:17:02 +0200

    feat:Migrate AccountSeeder from Laravel to AccountFixture in Symfony

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
