0bca345dcd943589d461d0e8022a830d05eaaadb
root <root@DESKTOP-GAVDRN5.localdomain>
2026-09-14 15:27:35 +0200

    feat(fee): add CSV export button

--- src/Controller/Admin/FeeCrudController.php
+++ src/Controller/Admin/FeeCrudController.php
@@ -7,11 +7,16 @@ namespace App\Controller\Admin;
 use App\Entity\Fee;
 use App\Enum\TypeFee;
 use DateTimeImmutable;
+use DateTimeInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Factory\FilterFactory;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -21,6 +26,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\DateTimeFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\NumericFilter;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 final class FeeCrudController extends AbstractCrudController
 {
@@ -88,7 +94,73 @@ final class FeeCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
-        return $actions;
+        return $actions
+            ->add(
+                Crud::PAGE_INDEX,
+                Action::new('export', 'CSV Export', 'fa fa-file-csv')
+                    ->createAsGlobalAction()
+                    ->linkToCrudAction('export')
+            );
+    }
+
+    public function export(AdminContext $context): StreamedResponse
+    {
+        $search = $context->getSearch();
+        if (null === $search) {
+            throw new \LogicException('The CSV export can only be used from the fee index page.');
+        }
+
+        $fields = new FieldCollection($this->configureFields(Crud::PAGE_INDEX));
+        $filters = $this->container->get(FilterFactory::class)->create(
+            $context->getCrud()->getFiltersConfig(),
+            $fields,
+            $context->getEntity(),
+        );
+        $queryBuilder = $this->createIndexQueryBuilder(
+            $search,
+            $context->getEntity(),
+            $fields,
+            $filters,
+        );
+
+        $response = new StreamedResponse(function () use ($queryBuilder): void {
+            $output = fopen('php://output', 'wb');
+            if (false === $output) {
+                throw new \RuntimeException('Unable to open the CSV output stream.');
+            }
+
+            fputcsv($output, [
+                'id',
+                'transfer',
+                'type',
+                'name',
+                'rate',
+                'amount',
+                'created_at',
+                'updated_at',
+            ]);
+
+            /** @var Fee $fee */
+            foreach ($queryBuilder->getQuery()->toIterable() as $fee) {
+                fputcsv($output, [
+                    $fee->getId(),
+                    $fee->getTransfer()?->getReference() ?? '',
+                    $fee->getType()->value,
+                    $fee->getName() ?? '',
+                    $fee->getRate(),
+                    $fee->getAmount(),
+                    $fee->getCreatedAt()?->format(DateTimeInterface::ATOM) ?? '',
+                    $fee->getUpdatedAt()?->format(DateTimeInterface::ATOM) ?? '',
+                ]);
+            }
+
+            fclose($output);
+        });
+
+        $response->headers->set('Content-Type', 'text/csv; charset=UTF-8');
+        $response->headers->set('Content-Disposition', 'attachment; filename="fees.csv"');
+
+        return $response;
     }
 
     public function createEntity(string $entityFqcn): Fee
